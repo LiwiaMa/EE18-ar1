f99b06a6bc85fafdfa6e84ac1cbff96edaccf884
Liwia Matuszczak <liwia.matuszczak@learnet.se>
2020-01-13 14:07:41 +0100

    repetition,spel

---
+++ javascript/6-labbar/spel.js
@@ -0,0 +1,21 @@
+/* Kända personer och händelser 
+
+Nobels fredspris går till sydafrikanske biskopen Desmond Tutu.
+
+Tage Erlander blir populär när han i TV berättar vitsen som avslutas med "huka er gubbar och kärringar för nu laddar han om" 
+*/
+
+/* Prylar, nyheter och uppfinningar 
+
+Myndigheten Kongl. Patentbyrån öppnar sina dörrar för allmänheten.aktiv
+
+Kaknästornhet står klart
+
+De första mynten präglas i Sigtuna
+*/
+
+ 
+
+/* Sport, fritid och blandad */
+
+/* Underhållning */
